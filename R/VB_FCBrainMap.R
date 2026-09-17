5e133f78270217862a09465484638a7abd0bb4bc
Damon Pham <damondpham@gmail.com>
2026-09-17 11:46:59 +0700

    update INLA link

--- R/VB_FCBrainMap.R
+++ R/VB_FCBrainMap.R
@@ -168,7 +168,7 @@ VB_FCBrainMap <- function(
   #   if (!requireNamespace("INLA", quietly = TRUE)) {
   #     stop(
   #       "Package \"INLA\" needed to for spatial ESS correction ",
-  #       "Please install it at https://www.r-inla.org/download-install.",
+  #       "Please install it at https://www.r-inla.org/download/.",
   #       call. = FALSE
   #     )
   #   }
