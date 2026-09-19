e6cd9f21fa9c43b71340921ea8f3dcec9697142c
Damon Pham <damondpham@gmail.com>
2026-09-19 13:28:12 +0700

    fix norm_BOLD call

--- tests/testthat/test-misc.R
+++ tests/testthat/test-misc.R
@@ -1,6 +1,6 @@
 test_that("Data normalization completes", {
   nv <- 1000
   nt <- 170
-  x <- BayesBrainMap:::norm_BOLD(matrix(rnorm(nv*nt), nrow=nv))
+  x <- fMRItools::norm_BOLD(matrix(rnorm(nv*nt), nrow=nv), scale_by="sd", scale_sm_FWHM=0)
   testthat::expect_true(all(dim(x) == c(nv, nt)))
 })
